c30ed71e196dfac23d3d8d3f106eff5fdd0946f1
Josh Graber <josh.graber.85@gmail.com>
2016-03-24 21:09:14 -0400

    Add config constants defining the names of the main, browser, ambient, and definition folders

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -6,6 +6,10 @@ export const CONFIG_FILE = `${PROJECT_NAME}.json`
 export const TYPINGS_DIR = PROJECT_NAME
 export const DTS_MAIN_FILE = 'main.d.ts'
 export const DTS_BROWSER_FILE = 'browser.d.ts'
+export const MAIN_TYPINGS_DIR = 'main'
+export const BROWSER_TYPINGS_DIR = 'browser'
+export const DEFINITIONS_DIR = 'definitions'
+export const AMBIENT_DEFINITIONS_DIR = 'ambient'
 
 export const HOMEPAGE = 'https://github.com/typings/typings'
 export const ISSUES_HOMEPAGE = 'https://github.com/typings/typings/issues'

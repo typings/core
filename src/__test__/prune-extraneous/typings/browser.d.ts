3f23ad522e43c3c5e0f2d3da5f7daecf92d76b81
Josh Graber <josh.graber.85@gmail.com>
2016-03-25 13:37:55 -0400

    Commit test artifacts as they are post test run. This is a clean starting point for any test run.

--- src/__test__/prune-extraneous/typings/browser.d.ts
+++ src/__test__/prune-extraneous/typings/browser.d.ts
@@ -1,4 +1,2 @@
-/// <reference path="browser/ambient/extraneous/index.d.ts" />
-/// <reference path="browser/definitions/extraneous/index.d.ts" />
 /// <reference path="browser/ambient/test/index.d.ts" />
 /// <reference path="browser/definitions/test/index.d.ts" />
--- src/__test__/prune-prod/typings/main.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="main/ambient/extraneous/index.d.ts" />
-/// <reference path="main/definitions/extraneous/index.d.ts" />
-/// <reference path="main/ambient/test/index.d.ts" />
-/// <reference path="main/definitions/test/index.d.ts" />

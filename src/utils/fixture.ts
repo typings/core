54384357982f4539abcec5e109b349ab315b83f1
Homa Wong <homawong@gmail.com>
2016-06-13 01:22:54 -0700

    Add fixture test, fix require cache
    Fix test-spec and test-cov to use -P instead of -p

---
+++ src/utils/fixture.ts
@@ -0,0 +1,27 @@
+import test = require('blue-tape')
+import { join } from 'path'
+
+export interface FixtureTest {
+  (name: string, fixtureName: string, cb: (t: test.Test, path: string) => any): test.Test
+  only(name: string, fixtureName: string, cb: (t: test.Test, path: string) => any): test.Test
+  skip(name: string, fixtureName: string, cb: (t: test.Test, path: string) => any): test.Test
+}
+
+export function fixture(path: string): FixtureTest {
+  function curry(testfn: (name: string, cb: test.TestCase) => any) {
+    return (
+      title: string,
+      fixtureName: string,
+      cb: (t: test.Test, path: string) => any
+    ) => {
+      const fixturePath = join(path, fixtureName)
+      return testfn(`${title} ${fixtureName}`, t => {
+        return cb(t, fixturePath)
+      })
+    }
+  }
+  let result: any = curry(test)
+  result.only = curry(test.only)
+  result.skip = curry(test.skip)
+  return result as FixtureTest
+}
